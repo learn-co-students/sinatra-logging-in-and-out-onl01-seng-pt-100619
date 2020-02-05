a3dbf47627c17a97ee328b741bca49c8308f1378
Kevin Hayden <kevin.fhayden@gmail.com>
2020-02-05 22:48:01 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,11 @@
 class Helpers
+  #These methods make it cleaner to add logic to our views!
+  def self.current_user(session)
+    @user = User.find(session[:user_id])
+  end
+  
+  def self.is_logged_in?(session)
+    !!session[:user_id]
+  end
+  #only call these methods in the account.erb view
 end
