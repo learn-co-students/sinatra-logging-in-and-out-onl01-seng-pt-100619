78a845ba0e26e3379c5a25f2b5b05c6ad565a41f
kelly r maass <kmaass77627@gmail.com>
2020-02-09 03:17:30 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 ENV['SINATRA_ENV'] ||= "development"
 
+
+configure :development do
+  set :database, 'sqlite3:db/users.db'
+end
+
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
